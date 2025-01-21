da7fb8881af6def5df63be34cdff0e7baf1f6efe
thanhhhtruc <164887514+thanhhhtruc@users.noreply.github.com>
2025-01-21 14:56:57 +0700

    Add transactions table with initial data

---
+++ src/app/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Header from "./_components/section-header";
+import Hero from "./_components/section-hero";
+import CompaniesList from "./_components/section-companies-list";
+import MockAnalysis from "./_components/section-mock-analysis";
+import CTASection from "./_components/section-features";
+import Footer from "./_components/section-footer";
+import LandingPage from "./_components/landing-page";
+
+
+async function Page() {
+  return (
+    <main className="min-h-screen overflow-x-hidden">
+      <Header />
+      <Hero />
+      <div className="flex flex-col gap-40 mt-10 mb-10">
+        <CompaniesList />
+        <MockAnalysis />
+        <LandingPage />
+        <CTASection />
+      </div>
+      <Footer />
+    </main>
+  );
+}
+
+export default Page;
